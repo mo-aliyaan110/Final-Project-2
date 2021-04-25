76679e191fa877f373bfe94af968abdf8ec05fed
mo-aliyaan110 <mohammed007aliyan@gmail.com>
2021-04-25 15:15:37 +0530

    apiis ready

---
+++ app.js
@@ -0,0 +1,140 @@
+const express = require('express')
+const app = express();
+const port = 7500;
+const mongo = require('mongodb');
+const mongoClient = mongo.MongoClient;
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'FinalApp';
+const chalk = require('chalk');
+const morgan = require('morgan');
+const fs = require('fs');
+let database;
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(morgan('tiny',{
+    stream:fs.createWriteStream('mylogs.logs', {flags:'a'})
+}))
+
+// to see the homepage i.e / or when first time you start the server
+app.get('/', (req,res) =>{
+    
+        res.send("<div><a href='http://localhost:9900/location' target='_blank' > Location </a> <br> <br> <a href='http://localhost:9900/cuisine' target='_blank'> Cuisine </a> <br/><br/> <a href='http://localhost:9900/mealtype' target='_blank'> Mealtype </a> <br/><br/> <a href='http://localhost:9900/restaurant' target='_blank'> Restaurant </a>   </div>")
+    
+})
+
+// to see all the cities
+app.get('/location', (req,res) =>{
+    database.collection('city').find({}).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result) 
+    })
+})
+
+// to see all the mealtypes
+app.get('/mealtype', (req,res) =>{
+    database.collection('mealtype').find({}).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result) 
+    })
+})
+
+// to see all the restaurants
+app.get('/restaurant', (req,res) =>{
+    // restaurants on the basis of city.
+    var query = {};
+    if(req.query.city){
+        query = {city:req.query.city}
+    }
+    // restaurants on basis of mealtype
+    else if (req.query.mealtype){
+        query = {'type.mealtype':req.query.mealtype}
+    }
+    // restaurant on basis of cuisine
+
+    else if(req.query.cuisine){
+        query = {'Cuisine.cuisine':req.query.cuisine}
+    }
+
+    database.collection('restaurant').find(query).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result) 
+    })
+})
+
+
+// to see all the orders
+app.get('/orders', (req,res)=>{
+    database.collection('orders').find({}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// post API to place the order in a form
+// check this post api in POST man
+app.post('/placeorder', (req,res)=>{
+    database.collection('orders').insertOne(req.body,(err,result) =>{
+        if (err){
+            console.log(err)
+        }
+        else{
+            res.send('Data Added')
+        }
+    }
+
+    )
+})
+
+
+
+
+//to see all the cuisines 
+app.get('/cuisine', (req,res) =>{
+    database.collection('cuisine').find({}).toArray((err, result) =>{
+        if(err) throw err;
+        res.send(result) 
+    })
+})
+
+// listing API here the mealtype will be default and on basis of City and cuisine you get the restaurant
+
+app.get('/restaurant', (req,res)=>{
+    var query = "http://localhost:7500/restaurant?mealtype";
+    if(req.query.mealtype){
+        query = {'type.mealtype':req.query.mealtype}
+    }
+    else if (req.query.city){
+        query = {city:req.query.city}
+    }
+    else if (req.query.cuisine){
+        query = {'Cuisine.cuisine':req.query.cuisine}
+    }
+    // else if (req.query.city && req.query.cuisine){
+    //     query = {city:req.query.city} && {'Cuisine.cuisine':req.query.cuisine}
+    // }
+
+    database.collection('restaurant').find(query).toArray((err,result)=>{
+        if(err) throw err
+        res.send(result);
+    })
+    
+})
+
+
+
+
+
+mongoClient.connect(mongoUrl, (err,data) =>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        database = data.db(dbName)
+    }
+    app.listen(port, (err) =>{
+        if(err) throw err;
+        console.log(chalk.blue(`Server is running at port ${port}`))
+    })
+})
