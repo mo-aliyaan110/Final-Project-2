9936d2dbccfd80b53ce3b7665b00871668b93b4c
mo-aliyaan110 <mohammed007aliyan@gmail.com>
2021-04-30 21:09:14 +0530

    filters need to be done

--- Frontend/src/home/home.js
+++ Frontend/src/home/home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Search from './search';
 import QuickSearch from './quickSearchApi';
 
-const Home = () =>{
+const Home = (props) =>{
+   const sendTorest = (data) =>{
+        props.history.push(`/rest/${data}`)
+    }
     return(
         <React.Fragment>
-            <Search/>
-            <QuickSearch/>
+            <Search Rid = {(data) => {sendTorest(data)}}/>
+            <QuickSearch  />
         </React.Fragment>
     )
 }
